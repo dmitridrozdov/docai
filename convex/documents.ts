0bba1426c9d2282dfdfdb90ef095a7ad54538480
dmitridrozdov <dm.drozdov@gmail.com>
2024-10-08 13:06:56 +1100

    Update documents.ts

--- convex/documents.ts
+++ convex/documents.ts
@@ -9,174 +9,20 @@ import {
     query,
   } from "./_generated/server";
   import { ConvexError, v } from "convex/values";
-  import { internal } from "./_generated/api";
-  // import OpenAI from "openai";
-  import { Id } from "./_generated/dataModel";
-  import { embed } from "./notes";
-  
-  // const openai = new OpenAI({
-  //   apiKey: process.env.OPENAI_API_KEY,
-  // });
-  
-  export async function hasAccessToDocument(
-    ctx: MutationCtx | QueryCtx,
-    documentId: Id<"documents">
-  ) {
-    const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
-  
-    if (!userId) {
-      return null;
-    }
-  
-    const document = await ctx.db.get(documentId);
-  
-    if (!document) {
-      return null;
-    }
-  
-    if (document.orgId) {
-      const hasAccess = await hasOrgAccess(ctx, document.orgId);
-  
-      if (!hasAccess) {
-        return null;
-      }
-    } else {
-      if (document.tokenIdentifier !== userId) {
-        return null;
-      }
-    }
-  
-    return { document, userId };
-  }
-  
-  export const hasAccessToDocumentQuery = internalQuery({
-    args: {
-      documentId: v.id("documents"),
-    },
-    async handler(ctx, args) {
-      return await hasAccessToDocument(ctx, args.documentId);
-    },
-  });
-  
-  export const generateUploadUrl = mutation(async (ctx) => {
-    return await ctx.storage.generateUploadUrl();
-  });
-  
-  export const hasOrgAccess = async (
-    ctx: MutationCtx | QueryCtx,
-    orgId: string
-  ) => {
-    const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
-  
-    if (!userId) {
-      return false;
-    }
-  
-    const membership = await ctx.db
-      .query("memberships")
-      .withIndex("by_orgId_userId", (q) =>
-        q.eq("orgId", orgId).eq("userId", userId)
-      )
-      .first();
-  
-    return !!membership;
-  };
-  
-  export const getDocuments = query({
-    args: {
-      orgId: v.optional(v.string()),
-    },
-    async handler(ctx, args) {
-      const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
-  
-      if (!userId) {
-        return undefined;
-      }
-  
-      if (args.orgId) {
-        const isMember = await hasOrgAccess(ctx, args.orgId);
-        if (!isMember) {
-          return undefined;
-        }
-  
-        return await ctx.db
-          .query("documents")
-          .withIndex("by_orgId", (q) => q.eq("orgId", args.orgId))
-          .collect();
-      } else {
-        return await ctx.db
-          .query("documents")
-          .withIndex("by_tokenIdentifier", (q) => q.eq("tokenIdentifier", userId))
-          .collect();
-      }
-    },
-  });
-  
-  export const getDocument = query({
-    args: {
-      documentId: v.id("documents"),
-    },
-    async handler(ctx, args) {
-      const accessObj = await hasAccessToDocument(ctx, args.documentId);
-  
-      if (!accessObj) {
-        return null;
-      }
-  
-      return {
-        ...accessObj.document,
-        documentUrl: await ctx.storage.getUrl(accessObj.document.fileId),
-      };
-    },
-  });
+
   
   export const createDocument = mutation({
     args: {
-      title: v.string(),
-      fileId: v.id("_storage"),
-      orgId: v.optional(v.string()),
+      title: v.string()
     },
     async handler(ctx, args) {
-      const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
-  
-      if (!userId) {
-        throw new ConvexError("Not authenticated");
-      }
-  
-      let documentId: Id<"documents">;
-  
-      if (args.orgId) {
-        const isMember = await hasOrgAccess(ctx, args.orgId);
-        if (!isMember) {
-          throw new ConvexError("You do not have access to this organization");
-        }
-  
-        documentId = await ctx.db.insert("documents", {
-          title: args.title,
-          fileId: args.fileId,
-          description: "",
-          orgId: args.orgId,
-        });
-      } else {
-        documentId = await ctx.db.insert("documents", {
-          title: args.title,
-          tokenIdentifier: userId,
-          fileId: args.fileId,
-          description: "",
-        });
-      }
-  
-      // await ctx.scheduler.runAfter(
-      //   0,
-      //   internal.documents.generateDocumentDescription,
-      //   {
-      //     fileId: args.fileId,
-      //     documentId,
-      //   }
-      // );
-    },
+      await ctx.db.insert("documents", {
+        title: args.title,
+      })
+    }
   });
-  
+
+
   // export const generateDocumentDescription = internalAction({
   //   args: {
   //     fileId: v.id("_storage"),
@@ -220,19 +66,19 @@ import {
   //   },
   // });
   
-  export const updateDocumentDescription = internalMutation({
-    args: {
-      documentId: v.id("documents"),
-      description: v.string(),
-      embedding: v.array(v.float64()),
-    },
-    async handler(ctx, args) {
-      await ctx.db.patch(args.documentId, {
-        description: args.description,
-        embedding: args.embedding,
-      });
-    },
-  });
+  // export const updateDocumentDescription = internalMutation({
+  //   args: {
+  //     documentId: v.id("documents"),
+  //     description: v.string(),
+  //     embedding: v.array(v.float64()),
+  //   },
+  //   async handler(ctx, args) {
+  //     await ctx.db.patch(args.documentId, {
+  //       description: args.description,
+  //       embedding: args.embedding,
+  //     });
+  //   },
+  // });
   
   // export const askQuestion = action({
   //   args: {
@@ -296,18 +142,18 @@ import {
   //   },
   // });
   
-  export const deleteDocument = mutation({
-    args: {
-      documentId: v.id("documents"),
-    },
-    async handler(ctx, args) {
-      const accessObj = await hasAccessToDocument(ctx, args.documentId);
+  // export const deleteDocument = mutation({
+  //   args: {
+  //     documentId: v.id("documents"),
+  //   },
+  //   async handler(ctx, args) {
+  //     const accessObj = await hasAccessToDocument(ctx, args.documentId);
   
-      if (!accessObj) {
-        throw new ConvexError("You do not have access to this document");
-      }
+  //     if (!accessObj) {
+  //       throw new ConvexError("You do not have access to this document");
+  //     }
   
-      await ctx.storage.delete(accessObj.document.fileId);
-      await ctx.db.delete(args.documentId);
-    },
-  });
+  //     await ctx.storage.delete(accessObj.document.fileId);
+  //     await ctx.db.delete(args.documentId);
+  //   },
+  // });
