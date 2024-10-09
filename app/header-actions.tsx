32c85b8b18e21ddb7e5352bbe980431070015037
dmitridrozdov <dm.drozdov@gmail.com>
2024-10-10 09:50:04 +1100

    Update header-actions.tsx

--- app/header-actions.tsx
+++ app/header-actions.tsx
@@ -1,15 +1,9 @@
 "use client";
 
 import { SignInButton, UserButton } from "@clerk/nextjs";
-import { Authenticated, Unauthenticated, AuthLoading, useMutation, useQuery } from "convex/react";
-import { api } from "@/convex/_generated/api";
-import { Button } from "@/components/ui/button";
+import { Authenticated, Unauthenticated, AuthLoading } from "convex/react";
 
 export function HeaderActions() {
-
-  const documents = useQuery(api.documents.getDocuments);
-  const createDocument = useMutation(api.documents.createDocument);
-
   return (
     <>
       <Unauthenticated>
@@ -18,12 +12,9 @@ export function HeaderActions() {
 
       <Authenticated>
         <UserButton />
-        <Button onClick={() => createDocument({title: "New Document"})}>click me</Button>
-        
       </Authenticated>
 
       <AuthLoading>Loading...</AuthLoading>
-      {documents?.map((doc) => <div>{doc.title}</div>)}
     </>
   );
 }
