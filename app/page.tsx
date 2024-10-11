02cec70bf9bead6e1e5669abbf9cb86c01b81a0e
dmitridrozdov <dm.drozdov@gmail.com>
2024-10-11 12:23:32 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 "use client" //check you might delete later
 
 import { SignInButton, UserButton } from "@clerk/clerk-react";
-import { Authenticated, Unauthenticated, useQuery } from "convex/react";
+import { Authenticated, Unauthenticated, AuthLoading, useMutation, useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
+import { DocumentCard } from "./document-card";
 
 export default function Home() {
+
+  const documents = useQuery(api.documents.getDocuments);
+  const createDocument = useMutation(api.documents.createDocument);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Unauthenticated>
@@ -12,6 +18,8 @@ export default function Home() {
       <Authenticated>
         <UserButton />
       </Authenticated>
+      <Button onClick={() => createDocument({title: "New Document"})}>click me</Button>
+      {documents?.map((doc) => <DocumentCard document={doc} />)}
     </main>
   );
 }
