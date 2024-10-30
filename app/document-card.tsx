3b51543cf5bfd50d3ef2aef7c278c045c15f041d
dmitridrozdov <dm.drozdov@gmail.com>
2024-10-30 16:25:46 +1100

    Update document-card.tsx

--- app/document-card.tsx
+++ app/document-card.tsx
@@ -9,6 +9,7 @@ import {
   } from "@/components/ui/card";
   import { Doc } from "@/convex/_generated/dataModel";
   import { Eye, Loader2 } from "lucide-react";
+  import Link from "next/link";
 
   export function DocumentCard({ document }: { document: Doc<"documents"> }) {
     return (
@@ -22,7 +23,9 @@ import {
         </CardContent>
         <CardFooter>
             <Button variant="secondary">
+            <Link href={`/dashboard/documents/${document._id}`}>
                 <Eye className="w-4 h-4" /> View
+            </Link>
             </Button>
         </CardFooter> 
         </Card>
