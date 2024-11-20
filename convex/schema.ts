0adb29165341a1202a8502b4a8a7804f4e2efdd9
dmitridrozdov <dm.drozdov@gmail.com>
2024-11-21 07:47:26 +1100

    Update schema.ts

--- convex/schema.ts
+++ convex/schema.ts
@@ -7,6 +7,12 @@ export default defineSchema({
     tokenIdentifier: v.optional(v.string()),
     fileId: v.id("_storage"),
   })
-  .index("by_tokenIdentifier", ["tokenIdentifier"])
+  .index("by_tokenIdentifier", ["tokenIdentifier"]),
+  chats: defineTable({
+    documentId: v.id("documents"),
+    tokenIdentifier: v.string(),
+    isHuman: v.boolean(),
+    text: v.string(),
+  }).index("by_documentId_tokenIdentifier", ["documentId", "tokenIdentifier"]),
   
 });
